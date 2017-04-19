7a894e8c6ff8b4295c8be876d4a9866f917002b2
PandaBasketteur <alexbourdon13@gmail.com>
2017-04-19 02:17:17 +0200

    v.2.0
    - Un salaire est versé en plus est versé en fonction du métier

--- paycheck_en/paycheck_sv.lua
+++ paycheck_en/paycheck_sv.lua
@@ -7,7 +7,12 @@ AddEventHandler('paycheck:salary', function()
   	local salary = 500
 	TriggerEvent('es:getPlayerFromId', source, function(user)
   	-- Adding money to the user
-  	user:addMoney((salary))
- 	TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Paycheck received :  + "..salary.." ~g~$")
+  	local user_id = user.identifier
+  	-- Query that retrieves the user's trade
+  	local executed_query = MySQL:executeQuery("SELECT salary FROM users INNER JOIN jobs ON users.job = jobs.job_id WHERE identifier = '@username'",{['@username'] = user_id})
+    local result = MySQL:getResults(executed_query, {'salary'})
+    local salary_job = result[1].salary
+  	user:addMoney((salary + salary_job))
+ 	TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Paycheck received :  + "..salary.."~g~€~s~~n~Paycheck Job received : + "..salary_job.." ~g~€")
  	end)
 end)
