a7ea8033f88c98e70e7685f3956a271a56ac016c
PandaBasketteur <alexbourdon13@gmail.com>
2017-04-19 09:52:26 +0200

    Add files via upload

--- paycheck_en/sample.sql
+++ paycheck_en/sample.sql
@@ -3,7 +3,7 @@
 --
 
 INSERT INTO `jobs` (`job_id`, `job_name`, `salary`)
-VALUES (1, 'Unemployed',500),(2, 'Cop', 1200),(3, 'Fireman', 1200);
+VALUES (1, 'Unemployed',500),(2, 'Cop', 1200),(3, 'Fireman', 1200),(4, 'Minor', 700),(5, 'Taxi driver', 1000);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
