4b810fa1b3efa4e0466cf2d8b8f6bab88ef752e4
PandaBasketteur <alexbourdon13@gmail.com>
2017-06-01 12:34:50 +0200

    Delete paycheck_cl.lua

--- paycheck_en/paycheck_cl.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function ()
-	while true do
-	Citizen.Wait(600000) -- Change this value for the frequency of paycheck (600000 = 10 minutes)
-		TriggerServerEvent('paycheck:salary')
-	end
-end)
