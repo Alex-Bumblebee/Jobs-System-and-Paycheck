0d4ab290d7f883c60c0f7284412525a57e5842be
PandaBasketteur <alexbourdon13@gmail.com>
2017-06-01 11:26:32 +0200

    Update mysql-async
    Use MySQL-Async : https://github.com/brouznouf/fivem-mysql-async

--- jobs-system/server.lua
+++ jobs-system/server.lua
@@ -1,5 +1,4 @@
-require "resources/essentialmode/lib/MySQL"
-MySQL:open("localhost", "gta5_gamemode_essential", "utilisateur", "mdp")
+require "resources/mysql-async/lib/MySQL"
 
 
 ---------------------------------- FUNCTIONS ----------------------------------
@@ -7,24 +6,20 @@ MySQL:open("localhost", "gta5_gamemode_essential", "utilisateur", "mdp")
 -- Fonction : Récupère le nom du travail
 -- Paramètre(s) : id = ID du travail
 function nameJob(id)
-  local executed_query = MySQL:executeQuery("SELECT job_name FROM jobs WHERE job_id = '@namejob'", {['@namejob'] = id})
-  local result = MySQL:getResults(executed_query, {'job_name'}, "job_id")
-  return result[1].job_name
+  return MySQL.Sync.fetchScalar("SELECT job_name FROM jobs WHERE job_id = @namejob", {['@namejob'] = id})
 end
 
 -- Fonction : Récupère le travail du joueur
 -- Paramètre(s) : player = Identifiant du joueur
 function whereIsJob(player)
-  local executed_query = MySQL:executeQuery("SELECT job FROM users WHERE identifier = '@identifier'", {['@identifier'] = player})
-  local result = MySQL:getResults(executed_query, {'job'}, "job")
-  return result[1].job
+  return MySQL.Sync.fetchScalar("SELECT job FROM users WHERE identifier = @identifier", {['@identifier'] = player})
 end
 
 -- Fonction : Mets à jour le travail du joueur
 -- Paramètre(s) : player = Identifiant du joueur, id = ID du travail
 function updatejob(player, id)
   local job = id
-  MySQL:executeQuery("UPDATE users SET `job`='@value' WHERE identifier = '@identifier'", {['@value'] = job, ['@identifier'] = player})
+  MySQL.Async.execute("UPDATE users SET `job`=@value WHERE identifier = @identifier", {['@value'] = job, ['@identifier'] = player})
 end
 
 ---------------------------------- EVENEMENT ----------------------------------
@@ -33,10 +28,10 @@ RegisterServerEvent('jobssystem:jobs')
 AddEventHandler('jobssystem:jobs', function(id)
   TriggerEvent('es:getPlayerFromId', source, function(user)
         local player = user.identifier
-        local nameJob = nameJob(id)
+        local jobName = nameJob(id)
         updatejob(player, id)
-        TriggerClientEvent("jobssystem:updateJob", source, nameJob)
-        TriggerClientEvent("es_freeroam:notify", source, "CHAR_MP_STRIPCLUB_PR", 1, "Mairie", false, "Votre métier est maintenant : ".. nameJob)
+        TriggerClientEvent("jobssystem:updateJob", source, jobName)
+        TriggerClientEvent("es_freeroam:notify", source, "CHAR_MP_STRIPCLUB_PR", 1, "Mairie", false, "Votre métier est maintenant : ".. jobName)
         -- ENGLISH VERSION : TriggerClientEvent("es_freeroam:notify", source, "CHAR_MP_STRIPCLUB_PR", 1, "Job", false, "Your Job is now : ".. nameJob)
   end)
 end)
@@ -45,8 +40,8 @@ AddEventHandler('es:playerLoaded', function(source)
   TriggerEvent('es:getPlayerFromId', source, function(user)
       local player = user.identifier
       local WIJ = whereIsJob(player)
-      local nameJob = nameJob(WIJ)
-      TriggerClientEvent("jobssystem:updateJob", source, nameJob)
+      local jobName = nameJob(WIJ)
+      TriggerClientEvent("jobssystem:updateJob", source, jobName)
     end)
 end)
 
